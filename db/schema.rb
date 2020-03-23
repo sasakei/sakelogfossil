fcd0d6810e7233d2f09e97a4f143090167838153
Kei Sasaki <z.kei.sasaki@G-PC-00516080.intra.gree-office.net>
2020-03-23 17:58:06 +0900

    docker

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_22_045417) do
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.bigint "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_comments_on_topic_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "topic_id"
@@ -46,6 +36,4 @@ ActiveRecord::Schema.define(version: 2020_03_22_045417) do
     t.string "password_digest"
   end
 
-  add_foreign_key "comments", "topics"
-  add_foreign_key "comments", "users"
 end
